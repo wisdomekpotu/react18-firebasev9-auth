5a7e94a32ff909a5ec440fa61c1de726b626a262
Jesse Boyer <imboyus@gmail.com>
2022-06-16 16:59:31 -0400

    Fix filename import, was breaking

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Form, Alert } from "react-bootstrap";
 import { Button } from "react-bootstrap";
-import { useUserAuth } from "../context/UserAuthContext";
+import { useUserAuth } from "../context/userAuthContext";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
