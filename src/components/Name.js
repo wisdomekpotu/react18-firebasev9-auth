367d1c4e0a0907771a6ccdba10e18977b7114042
user <sivtsovatanya1985@gmail.com>
2022-09-08 13:29:15 +0300

    - clear pr

--- src/components/Name.js
+++ src/components/Name.js
@@ -1,9 +1,31 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
+import {collection, getDocs} from "firebase/firestore";
+import {db} from "../firebase-config";
 
 
 const Name = () => {
 	const [userName, setUserName] = useState('');
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		getUsers()
+	}, [])
+
+	useEffect(() => {
+		console.log(users);
+	}, [users])
+
+	const getUsers = () => {
+		const userCollectionRef = collection(db, 'users');
+		getDocs(userCollectionRef).then(response => {
+			const usrs = response.docs.map(doc => ({
+				data: doc.data(),
+				id: doc.id,
+			}))
+			setUsers(usrs)
+		}).catch(error => console.log(error.message))
+	}
 
 	let navigate = useNavigate();
 	function handleClick() {
