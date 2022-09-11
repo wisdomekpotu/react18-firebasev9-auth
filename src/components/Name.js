273ccb4e680bcf027a89c0de096c7135825d4cf6
user <sivtsovatanya1985@gmail.com>
2022-09-11 15:37:14 +0300

    - implemented user creation

--- src/components/Name.js
+++ src/components/Name.js
@@ -12,6 +12,7 @@ const Name = () => {
 	const [userName, setUserName] = useState('');
 	const [userAge, setUserAge] = useState('');
 	const [toggle, setToggle] = useState(false);
+	const [userGender, setUserGender] = useState('male');
 	const saveUserName =  (e) => {
 		e.preventDefault();
 	if (userName === '') {
@@ -25,13 +26,13 @@ const Name = () => {
 	}
 	const saveUserAge =  (e) => {
 		e.preventDefault();
-		if (userAge === '' || userName === '') {
+		if (userAge === '' || userName === '' || userGender === '') {
 			return
 		}
-		addDoc(userCollectionRef, {userAge, userName})
+		addDoc(userCollectionRef, {userAge, userName, userGender})
 			.then(response => {
 				console.log(response);
-				navigate("/birthday");
+				navigate("/home");
 			}).catch(error => console.log(error.message))
 	}
 	const shovHide = () => {
@@ -39,7 +40,9 @@ const Name = () => {
 		let b = document.getElementById('age')
 		let c = document.getElementById('btn')
 		let d = document.getElementById('show')
-		d.style.display = 'none';
+		let g = document.getElementById('gender')
+		// d.style.display = 'none';
+		// g.style.display = 'none';
 		if (a.style.display === 'none') {
 			a.style.display = 'block'
 		} else if (a.style.display === 'block') {
@@ -55,35 +58,80 @@ const Name = () => {
 		} else if (c.style.display === 'block') {
 			c.style.display = 'none'
 		}
+		if (d.style.display === 'none') {
+			d.style.display = 'block'
+		} else if (c.style.display === 'block') {
+			d.style.display = 'none'
+		}
+		if (g.style.display === 'none') {
+			g.style.display = 'block'
+		} else if (c.style.display === 'block') {
+			g.style.display = 'none'
+		}
+
+	}
+	const hide1 = () => {
+		let d1 = document.getElementById('1');
+		let d2 = document.getElementById('2');
+		let d3 = document.getElementById('3');
+
+		d1.style.display = 'none';
+		d2.style.display = 'block';
+		d3.style.display = 'none';
+	}
+	const hide2 = () => {
+		let d1 = document.getElementById('1');
+		let d2 = document.getElementById('2');
+		let d3 = document.getElementById('3');
+
+		d1.style.display = 'none';
+		d2.style.display = 'none';
+		d3.style.display = 'block';
+	}
+	const hi = () => {
+		let d3 = document.getElementById('3');
+		console.log('hi')
 	}
 
 	return (
 		<div>
-			<input type='text'
-				   id='name'
-				   style={{display: 'block'}}
-				   placeholder='type username'
-				   value={userName}
-				   onChange={(e) => setUserName(e.target.value)}
-			/>
-			<input type='text'
-				   id='age'
-				   style={{display: 'none'}}
-				   placeholder='age'
-				   value={userAge}
-				   onChange={(e) => setUserAge(e.target.value)}
-			/>
-			<button onClick={shovHide}
-					id='show'
+			<div id='1' style={{display: 'block'}}>
+				<input type='text'
+					   id='name'
+					   style={{display: 'block'}}
+					   placeholder='type username'
+					   value={userName}
+					   onChange={(e) => setUserName(e.target.value)}
+				/>
+			<button onClick={hide1}>1</button></div>
+			<div id='2'style={{display: 'none'}}>2
+				<input type='number'
+					   id='age'
+					   style={{display: 'block'}}
+					   placeholder='age'
+					   value={userAge}
+					   onChange={(e) => setUserAge(e.target.value)}
+				/>
+				<button onClick={hide2}>2</button>
+			</div>
+			<div id='3'style={{display: 'none'}}>
+				<input type='text'
+					   id='gender'
+					   style={{display: 'block'}}
+					   placeholder='gender'
+					   value={userGender}
+					   onChange={(e) => setUserGender(e.target.value)}
+				/>
+				<button
+					onClick={saveUserAge}
+					id='btn'
 					style={{display: 'block'}}
-			>
-				NEXT
-			</button>
-			<button
-				onClick={saveUserAge}
-				id='btn'
-				style={{display: 'none'}}
-			>next main</button>
+				>next main</button></div>
+
+
+
+
+
 		</div>
 	)
 }
